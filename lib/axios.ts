83167d7d2b7149138afa5784a32fb0aca5d754ad
Takudzwa Allen <takudzwamushai@gmail.com>
2026-05-03 09:44:00 +0100

    fixed axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://hams-backend.vercel.app/api"
+    : "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
