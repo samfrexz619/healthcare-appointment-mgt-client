f77e7e64ebac8de62f8aacce62e72e15f5b23d38
Takudzwa Allen <takudzwamushai@gmail.com>
2026-05-08 08:45:52 +0100

    fixed env issue

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://hams-backend.vercel.app/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
   },
