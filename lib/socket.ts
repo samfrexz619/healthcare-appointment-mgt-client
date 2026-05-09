cdb35b39fed539852ef0556cdd95a4d09dfe7cae
Takudzwa Allen <takudzwamushai@gmail.com>
2026-05-09 13:12:02 +0100

    fixed socket url

--- lib/socket.ts
+++ lib/socket.ts
@@ -5,7 +5,7 @@ let socket: Socket | null = null;
 export const initSocket = (userId: string) => {
   if (socket?.connected) return;
 
-  socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:5000", {
+  socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "https://hams-backend.vercel.app", {
     withCredentials: true,
   });
 
